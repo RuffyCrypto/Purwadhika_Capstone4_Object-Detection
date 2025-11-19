155f5ebb8d973a8d624c56164fa459f870b56139
Luthfi Muzammil <luthfimuzammil@gmail.com>
2025-11-19 13:24:37 +0700

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 from pathlib import Path
 import subprocess, sys, os, glob
-from src.calorie_map import get_calorie_info
+from calorie_map import get_calorie_info
 
 st.set_page_config(layout='wide')
 st.title('FoodCal - YOLOv12 Inference (Stable)')
